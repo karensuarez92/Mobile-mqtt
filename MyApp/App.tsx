bb8a9efb191155a35946019cc3b221b379fd927e
karensuarez92 <karen.david.sami@gmail.com>
2025-10-07 15:01:28 -0600

    fix bottom

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -1,5 +1,5 @@
-import { StyleSheet, Platform } from 'react-native';
-import { SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import { StyleSheet } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import Navigation from './src/navigation/navigation';
 
 import { Colors } from './src/utils/helpers/colors';
@@ -18,7 +18,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.mainBg,
-    paddingTop: Platform.OS === 'android' ? 20: 0,
   },
 });
 
