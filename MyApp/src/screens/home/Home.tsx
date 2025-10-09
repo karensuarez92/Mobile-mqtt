aceb688f0fff63b45fab573c83f6ebd5f36e9909
karensuarez92 <karen.david.sami@gmail.com>
2025-10-09 15:08:16 -0600

    fix-home

--- MyApp/src/screens/home/Home.tsx
+++ MyApp/src/screens/home/Home.tsx
@@ -4,6 +4,7 @@ import { View, Text, StyleSheet } from 'react-native';
 import { Header } from '../Header/Header';
 import mqtt from 'mqtt';
 import { Colors } from '../../utils/helpers/colors';
+import Ionicons from '@react-native-vector-icons/ionicons';
 
 export const Home = () => {
   const [msg, setMsg] = useState('');
@@ -31,9 +32,35 @@ export const Home = () => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.card}>
-        <Text style={styles.text}>{msg}</Text>
+      <View>
+        <Text style={styles.text}>
+          Instituto Tecnologico Superior De Huichapan
+        </Text>
       </View>
+      <View style={styles.card2}>
+        <Ionicons name="location-outline" color={'white'} size={20} />
+        <Text style={styles.tex2}>El Saucillo</Text>
+      </View>
+      <View>
+        <View style={styles.sensorContainer}></View>
+        <Text style={styles.temp}>Temperatura</Text>
+        <Text style={styles.tempValue}>{msg ? `${msg} °C` : '--- °C'}</Text>
+        <View style={styles.iconRow}></View>
+      </View>
+      <Ionicons
+        name="thermometer-outline"
+        color={'white'}
+        size={60}
+        style={{ paddingTop: 10 }}
+      />
+      <Text style={styles.humed}>Humedad</Text>
+      <Text style={styles.tempValue}>{msg ? `${msg} °C` : '--- °C'}</Text>
+      <Ionicons
+        name="water-outline"
+        color={'white'}
+        size={60}
+        style={styles.icono}
+      />
     </View>
   );
 };
@@ -52,8 +79,55 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.cardBg,
   },
   text: {
-    fontSize: 20,
+    fontSize: 19,
     color: Colors.white,
     fontWeight: '800',
+    padding: 20,
+  },
+  tex2: {
+    fontSize: 25,
+    color: Colors.white,
+    fontWeight: '600',
+  },
+  card2: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    //backgroundColor: 'red',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  temp: {
+    fontSize: 22,
+    color: Colors.white,
+    fontWeight: '800',
+    backgroundColor: 'red',
+    //marginTop: 30,
+    //padding: 8,
+  },
+  humed: {
+    fontSize: 20,
+    color: Colors.white,
+    fontWeight: '500',
+    backgroundColor: 'red',
+    marginTop: 10,
+    padding: 10,
+  },
+  icono: {
+    //backgroundColor: 'blue',
+    paddingTop: 30,
+  },
+
+  sensorContainer: {
+    marginTop: 30,
+    alignItems: 'center',
+  },
+  iconRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  tempValue: {
+    fontSize: 30,
+    color: Colors.white,
+    fontWeight: '700',
   },
 });
--- MyApp/src/utils/helpers/colors.ts
+++ MyApp/src/utils/helpers/colors.ts
@@ -1,6 +1,6 @@
 export const Colors = {
   morado: '#800080',
   white: '#fff',
-  mainBg: '#26262E',
-  cardBg: '#35353B',
+  mainBg: '#73b5f3ff',
+  cardBg: '#171418ff',
 };
